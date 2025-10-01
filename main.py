86acdda50d1b682af9e1bb747bdf010cc9fb3310
Petyo Vodenicharov <petyo.voednicharov@pharmalex.com>
2025-10-01 21:38:48 +0300

    boos

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+get_book_text(path_to_file)
+    with open(path_to_file) as file:
+        content = file.read()
+
+    return content
+
+main()
+    get_book_text(books/frankenstein.txt)
+
+main()
